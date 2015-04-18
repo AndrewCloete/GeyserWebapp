4fb1bbab30aca0c37527a264f20a43831618cc04
Andrew Cloete <andrewhcloete@gmail.com>
2015-04-18 12:40:04 +0200

    Implemented SCL read, decode and display using heavy lifting
    	- Added SCLhttpClient.java which is an http utility for the webapp and
    allow the webapp to request content from an SCL. 
    	-- Parse Base64 content from scl reply using rexex
    	-- Decode base46content to obix using
    org.apache.commons.codec.binary.Base64 to decode
    	-- Parse obix using regex again
    
    Note however the OM2M has written extensive libraries to specifically
    just all of the above. You can find an example of this in
    SampleMonitor.java in IPUsample. e.g.
    
    	- SCL.doRequest(new
    RequestIndication(METHOD_CREATE,SCLID+"/applications/"+appId+"/containers",REQENTITY,new
    Container(DESC)));
    
    When I switch over to using MAVEN, I will incorporate these libraries.
    But for now the focus is on getting a minimalist working protoype so
    that I can understand what is going on under the hood i.e. appreciate
    the OM2M libraries.
    
    
    
    
    Note: Tomcat server did randomly stoped wordink for half an hour, and
    then all of a sudden it was fine. Very frustrating.

---
+++ GeyserWebapp/src/acza/sun/ee/geyserM2M/webapp/model/SCLhttpClient.java
@@ -0,0 +1,133 @@
+/* --------------------------------------------------------------------------------------------------------
+ * DATE:	18 Apr 2015
+ * AUTHOR:	Cloete A.H
+ * PROJECT:	M-Eng, Inteligent geyser M2M system.	
+ * ---------------------------------------------------------------------------------------------------------
+ * DESCRIPTION: 
+ * ---------------------------------------------------------------------------------------------------------
+ * PURPOSE: 
+ * ---------------------------------------------------------------------------------------------------------
+ */
+
+package acza.sun.ee.geyserM2M.webapp.model;
+
+import java.io.*;
+import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SCLhttpClient {
+	
+
+public static String get(String getURI){
+		
+		String content64reply = null;
+	
+		try{
+			URL url = new URL("http://" + getURI);
+
+			HttpURLConnection urlcon = (HttpURLConnection) url.openConnection();
+			urlcon.setRequestMethod("GET");	
+			urlcon.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=");
+			InputStream in = urlcon.getInputStream();
+			InputStreamReader inr = new InputStreamReader(in);
+			BufferedReader bin = new BufferedReader(inr);
+
+			/* ALTERNATIVE
+			InputStreamReader in = new InputStreamReader(url.openStream()); //(1)
+			BufferedReader bin = new BufferedReader(in);
+			 */
+			
+			StringBuilder builder = new StringBuilder();
+			String line;
+			while((line = bin.readLine()) != null){
+				builder.append(line);
+			}
+			
+			content64reply =  parseSCLreply(builder.toString());
+			
+
+		}catch (MalformedURLException e){
+			System.out.println(e);
+		}catch (IOException e2){
+			System.out.println(e2);
+		}
+		
+		return content64reply;
+	}	
+	
+	
+	public static void post(String postURI, String postData){
+		try{
+			URL url = new URL("http://" + postURI);
+			HttpURLConnection urlcon = (HttpURLConnection) url.openConnection();
+			urlcon.setRequestMethod("POST");
+			urlcon.setRequestProperty("Content-type", "text/html");
+			urlcon.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=");
+			urlcon.setDoOutput(true); //(2)
+			urlcon.setDoInput(true);
+			PrintWriter pout = new PrintWriter(new OutputStreamWriter(urlcon.getOutputStream(), "8859_1"), true);
+			pout.print(postData);
+			pout.flush();
+
+			InputStream in = urlcon.getInputStream();
+			InputStreamReader inr = new InputStreamReader(in);
+			BufferedReader bin = new BufferedReader(inr);
+
+			String line;
+			while((line = bin.readLine()) != null){
+				System.out.println(line); //Debug reply
+			}
+
+		}catch (MalformedURLException e){
+			System.out.println(e);
+		}catch (IOException e2){
+			System.out.println(e2);
+		}
+	
+	}
+	
+	
+	private static String parseSCLreply(String sclXML)
+	{
+		//(1)
+		Pattern r1 = Pattern.compile("(?<=<om2m:content xmime:contentType=\"application\\/xml\">).*(?=<\\/om2m:content>)", Pattern.DOTALL);
+		Matcher m1 = r1.matcher(sclXML);
+		
+		if (m1.find( )) {
+			return m1.group(0);
+			
+		} else {
+			return "SCL: NO MATCH 1";
+		}
+	}
+	
+	
+	public static String parseInstanceContent(String obix, String name){
+		/*
+		 * Regex geyserID, Temp and element
+		 */
+		
+		//(1)
+		Pattern r1 = Pattern.compile("(?<=<str val=\").{2,15}(?=\" name=\"" + name +"\")", Pattern.DOTALL);
+		Matcher m1 = r1.matcher(obix);
+		
+		if (m1.find( )) {
+			return m1.group(0);
+			
+		} else {
+			return "oBIX: NO MATCH 1";
+		}
+	}
+	
+	
+}
+
+/*
+ * ---------------------------------------------------------------------------------------------------------
+ * NOTES:
+ * 
+ * (1)
+ * Pattern r = Pattern.compile("(?<=whateverinfront).*(?=whateverattheback)")
+ * ---------------------------------------------------------------------------------------------------------
+ */
--- GeyserWebapp/src/acza/sun/ee/geyserM2M/webapp/web/GeyserServlet.java
+++ GeyserWebapp/src/acza/sun/ee/geyserM2M/webapp/web/GeyserServlet.java
@@ -1,4 +1,17 @@
+/* --------------------------------------------------------------------------------------------------------
+ * DATE:	17 Apr 2015
+ * AUTHOR:	Cloete A.H
+ * PROJECT:	M-Eng, Inteligent geyser M2M system.	
+ * ---------------------------------------------------------------------------------------------------------
+ * DESCRIPTION: 
+ * ---------------------------------------------------------------------------------------------------------
+ * PURPOSE: 
+ * ---------------------------------------------------------------------------------------------------------
+ */
+
 package acza.sun.ee.geyserM2M.webapp.web;
+
+import acza.sun.ee.geyserM2M.webapp.model.SCLhttpClient;
 
 import java.io.IOException;
 
@@ -9,12 +22,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.codec.binary.Base64;
+
 /**
  * Servlet implementation class GeyserServlet
  */
 @WebServlet(description = "Web version of GeyserSense Android application", urlPatterns = { "/GeyserServlet" })
 public class GeyserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,9 +45,20 @@ public class GeyserServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("geyserID", "mock: sim_geyser_1");
-		request.setAttribute("internalTemp", "mock: 45.5");
-		request.setAttribute("elementState", "mock: true");
+		
+		final String GEYSER_ID = "sim_geyser_1";
+		final String CONTAINER_ID = "DATA";
+		final String CONTENT_URI = "localhost:8181/om2m/gscl/applications/" + GEYSER_ID + "/containers/" + CONTAINER_ID + "/contentInstances/latest";
+		
+		String content64 = SCLhttpClient.get(CONTENT_URI);
+		//System.out.println(content64);//Debug reply
+		
+		String decoded = new String(Base64.decodeBase64(content64.getBytes()));
+		//System.out.println(decoded);
+		
+		request.setAttribute("geyserID", SCLhttpClient.parseInstanceContent(decoded, "appId"));
+		request.setAttribute("internalTemp", SCLhttpClient.parseInstanceContent(decoded, "Internal Temperature"));
+		request.setAttribute("elementState", SCLhttpClient.parseInstanceContent(decoded, "ElementState"));
 		
 		RequestDispatcher view = request.getRequestDispatcher("/geyserstatus.jsp");
 		view.forward(request, response);
@@ -44,3 +72,11 @@ public class GeyserServlet extends HttpServlet {
 	}
 
 }
+
+
+/*
+ * ---------------------------------------------------------------------------------------------------------
+ * NOTES:
+ * 
+ * ---------------------------------------------------------------------------------------------------------
+ */
